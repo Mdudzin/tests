974cc256396e35734b68787f3036053386e2914c
Dudzin <maciej.dudzin@protonmail.com>
2024-01-16 19:52:44 +0100

    small refactor

--- src/test/java/com/gitlab/rmarzec/task/Task2Test.java
+++ src/test/java/com/gitlab/rmarzec/task/Task2Test.java
@@ -8,16 +8,9 @@ public class Task2Test extends BaseTests {
 
   @Test
   public void printAllAvailableLanguages() {
-    //Inicjalizacja Page Objectów
     WikiPage wikiPage = new WikiPage(webDriver);
-
-    //1. przejście pod adres https://pl.wikipedia.org/wiki/Wiki
     navigateTo("https://pl.wikipedia.org/wiki/Wiki");
-
-    //2. kliknięcie przycisku z listą dostępnych języków
     wikiPage.clickLanguageButton();
-
-    //3. wypisanie wszystkich dostępnych języków z listy
     wikiPage.waitForLinks(webDriver);
     wikiPage.printAvailableLanguages();
   }
--- src/test/java/com/gitlab/rmarzec/task/Task3Test.java
+++ src/test/java/com/gitlab/rmarzec/task/Task3Test.java
@@ -12,50 +12,20 @@ public class Task3Test extends BaseTests {
 
   @Test
   public void navigateFromGoogleToW3Schools() {
-    //Inicjalizacja Page Objectów
     GoogleHomePage googleHomePage = new GoogleHomePage(webDriver);
     W3schoolsPage w3schoolsPage = new W3schoolsPage(webDriver);
-
-    //1. Przejście pod adres https://www.google.com/
     navigateTo("https://www.google.com/");
-
-    //2. Akceptacja ciasteczek
     googleHomePage.acceptCookies();
-
-    //3. Wpisanie w polu wyszukiwania "HTML select tag - W3Schools"
     googleHomePage.fillSearchInput("HTML select tag - W3Schools");
-
-    //4. Kliknięcie „Szczęśliwy traf”
     googleHomePage.clickFeelingLuckyButton();
-
-    //5. Zweryfikowanie czy trafiliśmy na stronę https://www.w3schools.com/tags/tag_select.asp
-    //Jeśli nie, w konsoli należy wypisać informację z aktualnym adresem oraz przejść na podany
-    //powyżej adres.
     checkIfCurrentUrlEquals("https://www.w3schools.com/tags/tag_select.asp");
-
-    //6. Akceptacja ciasteczek
     w3schoolsPage.acceptCookies();
-
-    //7. Uruchomienie pierwszego przykładu przyciskiem "Try it Yourself"
     w3schoolsPage.tryItYourself();
-
-    //Przełączenie się na nowo otwartą kartę
     switchToNewTab();
-
-    //8. Pobranie WebElementu, który zawiera treść nagłówka (The select element)
-    //9. Wypisanie nagłówka w konsoli
     w3schoolsPage.printH1Field();
-
-    //Przełączenie się do ramki
     switchToFrame("iframeResult");
-
-    //10. Wybranie z rozwijanej listy „Opel”
     Select cars = w3schoolsPage.selectCar("Opel");
-
-    //11. Pobranie wybranego elementu do WebElementu
     WebElement selectedOption = cars.getFirstSelectedOption();
-
-    //11. Wypisanie w konsoli tekstu oraz wartości „value” (Opel, opel)
     System.out.println(selectedOption.getText() + ", " + selectedOption.getDomAttribute("value"));
   }
 
